fbf3288cb5d361e05c1909b438ecc07716ed9eb3
João Pedro Azevedp <113111839+joaostazevedo172@users.noreply.github.com>
2024-08-12 17:47:34 -0300

    Update conexao.js

--- conexao.js
+++ conexao.js
@@ -1,4 +1,8 @@
+var express = require('express');
 var mysql = require('mysql');
+
+var app = express();
+var port = process.env.PORT || 3000; // Render geralmente define a porta via variável de ambiente
 
 var con = mysql.createConnection({
   host: "sql10.freemysqlhosting.net",
@@ -9,4 +13,12 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+});
+
+app.get('/', function(req, res) {
+  res.send('Connected to the database!');
+});
+
+app.listen(port, function() {
+  console.log(`Server is running on port ${port}`);
 });
